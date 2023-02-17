c86da3babf770da9fb82329f7aa743212fc34bf9
baek1008 <junu0810@hanmail.net>
2023-02-17 14:03:16 +0900

    design 요규사항 완료

--- client/src/components/student/RoomList.js
+++ client/src/components/student/RoomList.js
@@ -14,6 +14,7 @@ function RoomList({ roomList }) {
         // 업로드 후 파일명 출력
     };
 
+    console.log(roomList)
     return (
         <div>
             {
@@ -26,10 +27,14 @@ function RoomList({ roomList }) {
                             <div key={el.uuid} style={{ 'border': 'solid 1px #000' }}>
                                 <p>{el.created_at}</p>
                                 <h4>{el.class_name}</h4>
-                                <button>공유파일 다운로드</button>
                                 {/* <input type="file" ref={(el) => (inputRef.current[i]= el)} onChange={ (e) => { UploadFile(e.target.files, i) } } style={{'display' : 'none'}} /> */}
                                 {/* <p>{fileName[i]}<button onClick={()=>{ deleteFile(i) }}>삭제</button></p> */}
                                 {/* <button onClick={ () => { inputRef.current[i].click() } }>자료 업로드</button> */}
+                                <button>공유파일 다운로드</button>
+                            {
+                                el.progress === 0
+                                ? 
+                                <div>
                                 {fileName?? <p>{fileName}</p>}
                                 <button onClick={handleButtonClick}>파일 업로드</button>
                                 <input type="file"
@@ -37,6 +42,11 @@ function RoomList({ roomList }) {
                                     onChange={handleChange}
                                     style={{ display: "none" }} />
                                 <button>시작하기</button>
+                                </div>
+                                :
+                                <p>이미 종료된 회의입니다.</p>
+                            }
+
                             </div>
                         )
                     })
